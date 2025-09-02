3efc68219c65df8bcf1681dd8f2a1202ee521584
sarah <sarah.endres@pentlandfirth.com>
2025-09-02 09:34:00 +0200

    change paths

--- src/data_preprocessing/clean_complete.py
+++ src/data_preprocessing/clean_complete.py
@@ -13,9 +13,9 @@ from tqdm import tqdm
 
 #  gemuese_netz
 # ----------------------- HARD CODED CONFIG -----------------------
-IMAGES_ROOT = Path("~/opt/whizcart/shared/carrefour_classes/images/merci").expanduser()
-MASKS_ROOT  = Path("~/sarah/background_segmentation/dataset/pseudo_masks_mixed_merci").expanduser()
-OUT_ROOT    = Path("~/sarah/background_segmentation/dataset/mixed_pseudo_clean").expanduser()
+IMAGES_ROOT = Path("/opt/whizcart/shared/carrefour_classes/images/merci") #.expanduser()
+MASKS_ROOT  = Path("/sarah/background_segmentation/dataset/pseudo_masks_mixed_merci") #.expanduser()
+OUT_ROOT    = Path("/sarah/background_segmentation/dataset/mixed_pseudo_clean") #.expanduser()
 
 # IMAGES_ROOT = Path("/home/sarah/Documents/background_segmentation/dataset/pseudo_masks_224")
 # MASKS_ROOT = Path("/home/sarah/Documents/background_segmentation/dataset/images_pseudo_224")
