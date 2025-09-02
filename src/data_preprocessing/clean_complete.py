89c248fe462e4dfb340f0c37baea13bd65ca917e
sarah <sarah.endres@pentlandfirth.com>
2025-09-02 09:11:11 +0200

    change paths

--- src/data_preprocessing/clean_complete.py
+++ src/data_preprocessing/clean_complete.py
@@ -13,8 +13,8 @@ from tqdm import tqdm
 
 #  gemuese_netz
 # ----------------------- HARD CODED CONFIG -----------------------
-IMAGES_ROOT = Path("/opt/whizcart/shared/carrefour_classes/images/gemuese_netz").expanduser()
-MASKS_ROOT  = Path("/sarah/background_segmentation/dataset/pseudo_masks_mixed_gemuse").expanduser()
+IMAGES_ROOT = Path("/opt/whizcart/shared/carrefour_classes/images/merci").expanduser()
+MASKS_ROOT  = Path("/sarah/background_segmentation/dataset/pseudo_masks_mixed_merci").expanduser()
 OUT_ROOT    = Path("/sarah/background_segmentation/dataset/mixed_pseudo_clean").expanduser()
 
 SIDE_PADDING_RATIO = 0.1   # horizontal padding fraction
