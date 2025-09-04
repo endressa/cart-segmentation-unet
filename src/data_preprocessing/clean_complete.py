d5c33ef221e3c75e26eb6af483f4f94aba34ee6f
sarah <sarah.endres@pentlandfirth.com>
2025-09-04 09:54:07 +0200

    adjust cleaning script to overwrite unclean masks on server

--- src/data_preprocessing/clean_complete.py
+++ src/data_preprocessing/clean_complete.py
@@ -13,9 +13,9 @@ from tqdm import tqdm
 
 #  gemuese_netz
 # ----------------------- HARD CODED CONFIG -----------------------
-IMAGES_ROOT = Path("/opt/whizcart/shared/carrefour_classes/images/head_and_shoulders_sub_sarah").expanduser()
-MASKS_ROOT  = Path("~/sarah/background_segmentation/dataset/pseudo_masks_head_and shoulders").expanduser()
-OUT_ROOT    = Path("~/sarah/background_segmentation/dataset/mixed_pseudo_clean").expanduser()
+IMAGES_ROOT = Path("/home/ansible/sarah/background_segmentation/dataset/images_cleaned").expanduser()
+MASKS_ROOT  = Path("/home/ansible/sarah/background_segmentation/dataset/final_masks_cleaned").expanduser()
+OUT_ROOT    = Path("~/sarah/background_segmentation/dataset/final_masks_cleaned").expanduser()
 
 # IMAGES_ROOT = Path("/home/sarah/Documents/background_segmentation/head_and_shoulders_sarah/head_and_shoulders_sub_sarah")
 # MASKS_ROOT = Path("/home/sarah/Documents/background_segmentation/dataset/pseudo_masks_15/head_and_shoulders_sub_sarah")
