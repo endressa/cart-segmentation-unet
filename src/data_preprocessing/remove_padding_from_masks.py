a557eaeb528b1159e0257c8bb131bf63c39af671
sarah <sarah.endres@pentlandfirth.com>
2025-09-15 13:20:31 +0200

    adjust merci path

--- src/data_preprocessing/remove_padding_from_masks.py
+++ src/data_preprocessing/remove_padding_from_masks.py
@@ -393,12 +393,12 @@ def collect_pairs_from_roots(
 
 if __name__ == "__main__":
     IMAGE_ROOTS = [
-        Path("/opt/whizcart/shared/carrefour_classes/images/merci_raw"),
+        Path("/opt/whizcart/shared/carrefour_classes/images/merci_ra/raw"),
         Path("/opt/whizcart/shared/carrefour_classes/images/gemuese_netz/raw"),
         Path("/opt/whizcart/shared/carrefour_classes/images/head_and_shoulders_sub_sarah/raw"),
         Path("/opt/whizcart/shared/carrefour_classes/images/ariel_sarah/ariel_sarah/raw"),
     ]
-    
+
     # IMAGE_ROOTS = [Path("/opt/whizcart/shared/carrefour_classes/images")]
 
 
